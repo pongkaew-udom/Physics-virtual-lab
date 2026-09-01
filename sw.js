18f74048de6c66a01f322abe2c09d83832cf366f
pongkaew-udom <pongkaew@g.swu.ac.th>
2026-09-01 08:15:58 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11642325
+    "zoneId": 11698352
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
